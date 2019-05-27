c8b626d9528a55a349069519d6f925775a68a03c
ZT <3193152800@qq.com>
2019-05-27 14:46:21 +0800

    新项目

---
+++ WJ/WJ/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WJ
+//
+//  Created by ZhangTeng on 2019/5/27.
+//  Copyright © 2019 ZhangTeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ WJ/WJ/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  WJ
+//
+//  Created by ZhangTeng on 2019/5/27.
+//  Copyright © 2019 ZhangTeng. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
